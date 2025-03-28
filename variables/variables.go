0beaf97b2f668bf20271e051d555af44b4b024df
nishhcodes <issacneedscoffee@gmail.com>
2025-03-28 13:26:31 +0530

    Variables in Go!

---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	name := "Manish"
+	age := 24
+	isDeveloper := true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Developer:", isDeveloper)
+
+}
