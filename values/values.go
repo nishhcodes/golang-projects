388190e9bfd6e6258d6bf7cc4708caa6a5efa066
nishhcodes <issacneedscoffee@gmail.com>
2025-03-28 13:17:35 +0530

    Go values

---
+++ values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Example of a String
+	fmt.Println("Hola" + " I am " + "Manish!")
+	// Example of an Integer
+	fmt.Println("1 + 1 =", 1+1)
+	// Example of a Float
+	fmt.Println("7.0 / 3.0 ", 7.0/3.0)
+	// Example of a Boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
