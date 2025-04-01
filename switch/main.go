18afaf6383197ff569886f968f14a6febb3e0d1b
nishhcodes <issacneedscoffee@gmail.com>
2025-04-02 00:01:00 +0530

    Switch in Go!

--- switch/main.go
+++ switch/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 func main() {
     i := 12
@@ -15,4 +16,35 @@ func main() {
     default: 
         fmt.Println(i, "Not found!")
     }
+
+    switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+        fmt.Println("It's the Weekend!")
+    default:
+        fmt.Println("It's a Weekday!")
+    }
+
+    user := "Manish"
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("Good Morning!", user)
+    default: 
+        fmt.Println("Good Afternoon!", user)
+    }
+
+    whatAmI := func(i interface{}) {
+        switch t := i.(type) {
+        case bool:
+            fmt.Println("I'm a boolean")
+        case int: 
+            fmt.Println("I'm an integer!")
+        default:
+            fmt.Println("Don't know type \n", t)
+        }
+    }
+    
+    whatAmI(true)
+    whatAmI(2)
+    whatAmI("Hello, World!")
 }
